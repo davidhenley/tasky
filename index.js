7172a534d8131f1252f56e64f743c687bbc6f863
David Henley <henleydr@gmail.com>
2017-05-21 11:36:29 -0500

    Calculating Bounds

--- index.js
+++ index.js
@@ -18,10 +18,20 @@ app.on('ready', () => {
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
   tray = new Tray(iconPath);
 
-  tray.on('click', () => {
+  tray.on('click', (event, bounds) => {
+    const { x, y } = bounds;
+    const { width, height } = mainWindow.getBounds();
+
     if (mainWindow.isVisible()) {
       mainWindow.hide();
     } else {
+      const yPosition = process.platform === 'darwin' ? y : y - height;
+      mainWindow.setBounds({
+        x: x - width / 2,
+        y: yPosition,
+        height,
+        width
+      });
       mainWindow.show();
     }
   });
