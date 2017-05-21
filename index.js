1c161317579f20258413bfdf8597ec2af864cc66
David Henley <henleydr@gmail.com>
2017-05-21 16:39:24 -0500

    Refactor to TimerTray class

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const { app, BrowserWindow, Tray } = require('electron');
+const { app, BrowserWindow } = require('electron');
+const TimerTray = require('./app/TimerTray');
 
 let mainWindow;
-let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -16,23 +16,6 @@ app.on('ready', () => {
 
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-  tray = new Tray(iconPath);
-
-  tray.on('click', (event, bounds) => {
-    const { x, y } = bounds;
-    const { width, height } = mainWindow.getBounds();
-
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    } else {
-      const yPosition = process.platform === 'darwin' ? y : y - height;
-      mainWindow.setBounds({
-        x: x - width / 2,
-        y: yPosition,
-        height,
-        width
-      });
-      mainWindow.show();
-    }
-  });
+  
+  new TimerTray(iconPath, mainWindow);
 });
